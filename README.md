c73982bb1ed1200de10c17a99c3a5b7d09dcf3de
kyomi <me@kyomi.codes>
2023-04-26 21:49:11 -0300

    feat: create installation script

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# dotfiles
-My personal configuration dotfiles.
+# `dotfiles/bitterteriyaki`
+
+## Installing
+
+This guide assumes that you are using Arch Linux **or** macOS Big Sur. If you
+don't use either of these, you can still use the dotfiles, but you will need to
+install the dependencies manually.
+
+To install the dotfiles, you need to have [Chezmoi](https://chezmoi.io/)
+installed in your system. Check the documentation for instructions on how to
+install it.
+
+Once you have Chezmoi installed, run the `install.sh` script to setup some
+dependencies:
+
+```sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/bitterteriyaki/dotfiles/HEAD/install.sh)"
+```
+
+## Developing
+
+When developing for this repository, you can install Git hooks to ease your
+development. For that, you need to have [Poetry](https://python-poetry.org/)
+(1.4.2 or earlier) installed in your system.
+
+You can install the Git hooks by running the following commands:
+
+```sh
+poetry install
+poetry run pre-commit install
+```
+
+And that's it! Now, whenever you commit a change, the hooks will run and
+automatically format your code and check for any errors.
---
+++ install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Copyright (c) 2023-present kyomi
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#############
+# Variables #
+#############
+
+HB_URL="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+YAY_URL="https://aur.archlinux.org/yay.git"
+
+
+########################
+# Installation scripts #
+########################
+
+install_homebrew() {
+  if ! command -v brew &> /dev/null; then
+    echo -n "=> Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL $HB_URL)"
+    echo -e "=> Homebrew has been installed."
+  else
+    echo "=> Homebrew is already installed."
+  fi
+}
+
+install_yay() {
+  if ! command -v yay &> /dev/null; then
+    echo -n "=> Installing Yay..."
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    (cd /tmp/yay && makepkg -sirc)
+    echo -e "=> Yay has been installed."
+  else
+    echo "=> Yay is already installed."
+  fi
+}
+
+install_package_manager() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    install_homebrew
+  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    install_yay
+  else
+    echo "=> Unknown OS."
+  fi
+}
+
+###############
+# Main script #
+###############
+
+setup() {
+  echo -e "=> This script will setup kyomi's dotfiles on your system."
+  read -n 1 -r -s -p $'=> Press any key to continue or Ctrl+C to abort...\n'
+
+  install_package_manager
+}
+
+setup
