170baf63c8276d65a4da4ba0a2ff4822c8130fa1
kyomi <me@kyomi.dev>
2024-01-14 12:00:00 +0100

    feat(nvim): improve bracket colorizing

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -21,17 +21,38 @@ return {
     config = function()
       local ibl = require("ibl")
       local hooks = require("ibl.hooks")
+      local rd = require("rainbow-delimiters.setup")
 
-      local highlight = { "RainbowMuted" }
+      local highlight = {
+        "RainbowMuted",
+        "RainbowLove",
+        "RainbowGold",
+        "RainbowFoam",
+        "RainbowRose",
+        "RainbowPine",
+        "RainbowIris",
+      }
 
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
         vim.api.nvim_set_hl(0, "RainbowMuted", { fg = "#6e6a86" })
+        vim.api.nvim_set_hl(0, "RainbowLove", { fg = "#b4637a" })
+        vim.api.nvim_set_hl(0, "RainbowGold", { fg = "#ea9d34" })
+        vim.api.nvim_set_hl(0, "RainbowFoam", { fg = "#56949f" })
+        vim.api.nvim_set_hl(0, "RainbowRose", { fg = "#d7827e" })
+        vim.api.nvim_set_hl(0, "RainbowPine", { fg = "#286983" })
+        vim.api.nvim_set_hl(0, "RainbowIris", { fg = "#907aa9" })
       end)
 
       ibl.setup({
         indent = { char = "▏" },
         scope = { highlight = highlight },
       })
+      rd.setup({ highlight = highlight })
+
+      hooks.register(
+        hooks.type.SCOPE_HIGHLIGHT,
+        hooks.builtin.scope_highlight_from_extmark
+      )
     end,
   },
   --
@@ -41,5 +62,11 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VimEnter",
     opts = {},
+  },
+  --
+  -- Colorized brackets
+  --
+  {
+    "HiPhish/rainbow-delimiters.nvim",
   },
 }
