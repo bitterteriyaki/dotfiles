7cdb4877161435f04a7dd7b65a250304b1c4b5bc
kyomi <me@kyomi.dev>
2024-01-26 12:00:00 +0100

    feat(nvim): initial `nvim` configuration

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+local opt = vim.opt
+
+vim.g.mapleader = " "
+
+opt.fileencoding = "utf-8"
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.cursorline = true
+opt.number = true
+opt.numberwidth = 4
+opt.wrap = false
