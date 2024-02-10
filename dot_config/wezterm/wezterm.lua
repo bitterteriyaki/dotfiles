2adb1c43070ded3e59c8151e0a4267179ab565de
kyomi <me@kyomi.dev>
2024-02-10 12:00:00 +0100

    feat(wezterm): add window padding

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -13,4 +13,9 @@ config.animation_fps = 60
 config.leader = { key = "a", mods = "CTRL" }
 config.keys = require("keys")
 
+--
+-- Window padding
+--
+config.window_padding = require("window_padding")
+
 return config
