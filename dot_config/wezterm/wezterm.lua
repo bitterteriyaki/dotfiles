7ebef052f1771a4b27bd7fd432e57ab1b2a7df55
kyomi <me@kyomi.dev>
2024-02-11 12:00:00 +0100

    feat(wezterm): add initial keybinds

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+--
+-- Cursor style
+--
+config.default_cursor_style = "BlinkingBar"
+config.animation_fps = 60
+
+--
+-- Keybinds
+--
+config.leader = { key = "a", mods = "CTRL" }
+config.keys = require("keys")
+
+return config
