6895fc7afc9c2953ac97f5119ae71a40a21a5a70
kyomi <me@kyomi.dev>
2024-02-26 12:00:00 +0100

    feat(neovim): enable current line blame

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -4,7 +4,10 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup({
+        current_line_blame = true,
+        current_line_blame_formatter = "<author> (<author_time:%d/%m/%Y>) - <summary>"
+      })
     end,
   },
   -- indent guides
