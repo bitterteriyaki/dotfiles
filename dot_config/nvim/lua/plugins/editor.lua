e265808470d58e88022bfe6bd1f8b2aac6c7398a
kyomi <me@kyomi.dev>
2024-02-28 12:00:00 +0100

    feat(neovim): pass options to gitsigns with `opts` key

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -3,12 +3,10 @@ return {
   -- lets you interactively stage and unstage hunks in a commit
   {
     "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({
-        current_line_blame = true,
-        current_line_blame_formatter = "<author> (<author_time:%d/%m/%Y>) - <summary>"
-      })
-    end,
+    opts = {
+      current_line_blame = true,
+      current_line_blame_formatter = "<author> (<author_time:%d/%m/%Y>) - <summary>",
+    },
   },
   -- indent guides
   {
