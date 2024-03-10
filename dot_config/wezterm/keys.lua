01296a53a46f07237d7dfafe60b7f516ff1a9beb
kyomi <me@kyomi.dev>
2024-03-10 12:00:00 +0100

    feat(wezterm): add keybinds for resize panes

--- dot_config/wezterm/keys.lua
+++ dot_config/wezterm/keys.lua
@@ -11,4 +11,24 @@ return {
     mods = "LEADER|SHIFT",
     action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
   },
+  {
+    key = "h",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize { 'Left', 1 },
+  },
+  {
+    key = "j",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize { 'Down', 1 },
+  },
+  {
+    key = "k",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize { 'Up', 1 },
+  },
+  {
+    key = "l",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize { 'Right', 1 },
+  },
 }
