f7f1404dea112f79c65a6cc76ee2977f69eceb41
kyomi <me@kyomi.dev>
2024-03-11 12:00:00 +0100

    feat(neovim): add `presence.nvim` plugin

--- dot_config/nvim/lua/plugins/extras.lua
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -4,4 +4,10 @@ return {
     "wakatime/vim-wakatime",
     lazy = false,
   },
+  -- Discord Rich Presence
+  {
+    "andweeb/presence.nvim",
+    event = "BufEnter",
+    opts = { neovim_image_text = "Neovim" },
+  },
 }
