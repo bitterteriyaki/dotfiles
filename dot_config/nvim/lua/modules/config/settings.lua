cdd66afac623a627092158dff21e09238fe31feb
kyomi <me@kyomi.dev>
2024-03-15 15:05:54 -0300

    feat: remove some configurations

--- dot_config/nvim/lua/modules/config/settings.lua
+++ dot_config/nvim/lua/modules/config/settings.lua
@@ -2,8 +2,34 @@ local M = {}
 
 M.load_default_options = function()
   local default_options = {
+    backup = false,
+    clipboard = "unnamedplus",
+    completeopt = { "menuone", "noselect" },
+    cmdheight = 1,
+    conceallevel = 0,
+    fileencoding = "utf-8",
+    foldmethod = "manual",
+    folderexpr = "",
+    hidden = true,
+    hlsearch = true,
+    ignorecase = true,
+    mouse = "a",
+    pumheight = 10,
+    smartcase = true,
+    splitbelow = true,
+    splitright = true,
+    termguicolors = true,
+    title = true,
+    expandtab = true,
+    shiftwidth = 2,
+    tabstop = 2,
+    cursorline = true,
     number = true,
     numberwidth = 4,
+    signcolumn = "yes",
+    wrap = false,
+    scrolloff = 8,
+    sidescrolloff = 8,
   }
 
   for k, v in pairs(default_options) do
