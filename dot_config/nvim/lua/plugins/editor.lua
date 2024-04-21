fc12679aa0a97ad4adca8795c3bb91999fed6125
kyomi <me@kyomi.dev>
2024-04-21 12:00:00 +0100

    feat(nvim): add `git` signs plugin

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,7 +1,18 @@
+local blame_formatter = "<author> (<author_time:%d/%m/%Y) | <summary>"
+
 return {
   -- Fuzzy finder
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  -- Git signs
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_formatter = blame_formatter,
+      attach_to_untracked = true,
+    },
   },
 }
