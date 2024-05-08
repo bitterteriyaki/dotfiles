cfeae7f072cd41b702c362abd6fe54eb07cb496e
kyomi <me@kyomi.dev>
2024-05-08 12:00:00 +0100

    feat(nvim): add Discord Rich Presence plugin

--- dot_config/nvim/lua/plugins/extras.lua
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -8,5 +8,14 @@ return {
   {
     "github/copilot.vim",
   },
+  -- Discord Rich Presence
+  {
+    "andweeb/presence.nvim",
+    event = "BufEnter",
+    opts = {
+      neovim_image_text = "Neovim",
+      main_image = "file",
+    },
+  },
 }
 
