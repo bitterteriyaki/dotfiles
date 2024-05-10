0320d26aaa93b707d1778f38aac30629acd58867
kyomi <me@kyomi.dev>
2024-05-10 12:00:00 +0100

    feat(wezterm): add window padding

---
+++ dot_config/wezterm/window_padding.lua
@@ -0,0 +1,9 @@
+local horizontal_padding = 20
+local vertical_padding = 20
+
+return {
+  left = horizontal_padding,
+  right = horizontal_padding,
+  top = vertical_padding,
+  bottom = vertical_padding,
+}
