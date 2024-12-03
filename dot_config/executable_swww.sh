8e419046761cab463426fa3a146095012815a768
kyomi <me@kyomi.dev>
2024-12-03 20:16:30 -0300

    feat(swww): improve speed when setting the wallpaper

--- dot_config/executable_swww.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wallpaper="$HOME/.config/wallpaper.png"
-
-magick "$wallpaper" -coalesce -repage 0x0 -crop 3325x1870+0+0 +repage /tmp/left.png
-magick "$wallpaper" -coalesce -repage 0x0 -crop 2355x1331+3325+813 +repage /tmp/right.png
-
-swww img -o HDMI-A-1 /tmp/left.png
-swww img -o DVI-D-1 /tmp/right.png
